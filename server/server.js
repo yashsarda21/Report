4282cf95252df19476b78a610b60617268ede5fd
Pranav Sarda <sardap@Pranavs-MacBook-Air.local>
2024-12-25 22:28:57 +0530

    added in server file

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const cors =require("cors");
 const port = 5001;
 
 const corsOptions = {
-    origin: "http://localhost:5175",
+    origin: "https://reportui.vercel.app",
     methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
     credentials: true,
 }
