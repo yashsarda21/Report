0560d12ad784ff41e906d4aa62539a439e655d41
Pranav Sarda <sardap@Pranavs-MacBook-Air.local>
2024-12-25 23:20:01 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -1,50 +1,37 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
-
 const app = express();
+const cors =require("cors");
+
 const port = 5001;
 
-// Define CORS options
 const corsOptions = {
-    origin: ["*"], // Allowed origins
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD"], // Explicitly list methods
-    credentials: true, // Allow credentials (e.g., cookies, Authorization headers)
-    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
-};
-
-// Apply CORS middleware
+    origin: ["https://reportui.vercel.app"],
+    methods: ["GET, POST, PUT, DELETE, PATCH, HEAD"],
+    credentials: true,
+    allowedHeaders: 'Content-Type,Authorization',
+}
 app.use(cors(corsOptions));
 
-// Middleware for parsing JSON
 app.use(express.json());
 
-// Import routes and middleware
+
 const authRouter = require("./router/auth-router");
-const contactRoute = require("./router/contact-router");
-const powerBiRoute = require("./router/powerBi-router");
-const adminRoute = require("./router/admin-router");
+const conactRoute = require("./router/contact-router")
+const powerBiRoute = require("./router/powerBi-router")
+const adminRoute = require("./router/admin-router")
 const connectDB = require("./utils/db");
-const errorMiddleware = require("./middleware/error-middleware");
+const errorMiddleware = require("./middleware/error-middlware");
 
-// Routes
-app.use("/api/auth", authRouter);
-app.use("/api/form", contactRoute);
-app.use("/api/powerbi", powerBiRoute);
-app.use("/api/admin", adminRoute);
-
-// Error handling middleware
 app.use(errorMiddleware);
 
-// Connect to the database and start the server
-connectDB()
-    .then(() => {
-        app.listen(port, () => {
-            console.log(`Server is running at port ${port}`);
-        });
-    })
-    .catch((err) => {
-        console.error("Database connection failed", err);
-        process.exit(1);
-    });
+app.use("/api/auth" , authRouter);
+app.use("/api/form" , conactRoute);
+app.use("/api/powerbi" , powerBiRoute);
+app.use("/api/admin" , adminRoute);
 
+connectDB().then(() => { 
+    app.listen(port , () => {
+    console.log("server is running at 5001 port no");
+    });
+});
